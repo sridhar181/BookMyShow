1aeea0c0ef20d69c1da5e2367a6853441361093e
sridhar <sridharreddy1811@gmail.com>
2023-10-28 15:17:54 +0530

    coupon size increased

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const changeMode = () => {
 
 function onloadPage(){
     document.getElementById('coupon').style.visibility = 'visible';
-    document.getElementById('search').style.opacity = '0.7'
+    
+    
 }
 
 function closeCoupon(){
     document.getElementById('coupon').style.visibility = 'hidden';
+    document.body.style.opacity = '1';
 
 }
 
